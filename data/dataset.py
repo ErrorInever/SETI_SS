27dcc6e72b03ca12f384f3e5fde42640217e96b6
ErrorInever <errorinever@gmail.com>
2021-06-15 17:45:46 +0000

    add strong augmentation

--- data/dataset.py
+++ data/dataset.py
@@ -19,6 +19,15 @@ class SETIDataset(Dataset):
 
         self._transform = A.Compose([
             A.Resize(cfg.IMG_SIZE, cfg.IMG_SIZE),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.5),
+            A.Rotate(limit=180, p=0.7),
+            A.RandomBrightness(limit=0.6, p=0.5),
+            A.Cutout(
+                num_holes=10, max_h_size=12, max_w_size=12,
+                fill_value=0, always_apply=False, p=0.5
+            ),
+            A.ShiftScaleRotate(shift_limit=0.25, scale_limit=0.1, rotate_limit=0),
             ToTensorV2()
         ])
         self._resize_to_tensor = A.Compose([
